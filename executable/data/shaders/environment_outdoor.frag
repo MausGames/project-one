d4475ad2775111023b081b2b5e697f5cb8b95a5d
Martin Mauersics <revision@maus-games.at>
2014-08-22 17:58:11 +0200

    updated structure and engine to the most recent version, implemented outdoor class, added first few resource files

---
+++ executable/data/shaders/environment_outdoor.frag
@@ -0,0 +1,74 @@
+//////////////////////////////////////////////////////
+//*------------------------------------------------*//
+//| Part of Project One (http://www.maus-games.at) |//
+//*------------------------------------------------*//
+//| Released under the zlib License                |//
+//| More information available in the readme file  |//
+//*------------------------------------------------*//
+//////////////////////////////////////////////////////
+
+
+// shadow uniforms
+//uniform sampler2D u_s2ShadowTex;
+//uniform float u_fShadowVal;
+
+// shader input
+varying float v_fMix;
+//varying vec4 v_v4ShadowPos;
+
+
+void main()
+{
+    float fClampMix = clamp(v_fMix, 0.0, 1.0);
+    
+    vec3 v3Normal;
+    vec4 v4Texture;
+    if(fClampMix == 0.0)
+    {
+        v3Normal  = texture2D(u_as2Texture[3], v_av2TexCoord[0]).xyz;
+        v4Texture = texture2D(u_as2Texture[2], v_av2TexCoord[0]);
+    }
+    else if(fClampMix == 1.0)
+    {
+        v3Normal  = texture2D(u_as2Texture[1], v_av2TexCoord[0]).xyz;
+        v4Texture = texture2D(u_as2Texture[0], v_av2TexCoord[0]);
+    }
+    else
+    {
+        v3Normal  = mix(texture2D(u_as2Texture[3], v_av2TexCoord[0]).xyz, texture2D(u_as2Texture[1], v_av2TexCoord[0]).xyz, fClampMix);
+        v4Texture = mix(texture2D(u_as2Texture[2], v_av2TexCoord[0]),     texture2D(u_as2Texture[0], v_av2TexCoord[0]),     fClampMix);
+    }
+    
+    vec3 v3MathLightDir = normalize(v_av4LightDir[0].xyz);
+    vec3 v3BumpNormal   = normalize(v3Normal * 2.0 - 1.0);
+    float fBumpFactor   = dot(v3MathLightDir, v3BumpNormal); 
+    
+    vec3 v3MathViewDir = normalize(v_v3ViewDir);
+    vec3 v3RefNormal  = normalize((2.0 * fBumpFactor * v3BumpNormal) - v3MathLightDir); 
+    float fRefFactor  = max(0.0, dot(v3MathViewDir, v3RefNormal));
+    
+    float fShadow;
+    //if(u_fShadowVal > 0.0 && u_v4Color.a > 0.0)
+    //{
+    //    vec2 v2TexCoord = (gl_FragCoord.xy * u_v4Resolution.zw) * 2.0 - 1.0;
+    //
+    //    vec3 v2ShadowCoord = v_v4ShadowPos.xyz/v_v4ShadowPos.w;
+    //    float fDepth       = texture2D(u_s2ShadowTex, v2ShadowCoord.st).r;
+    //    fShadow            = (fDepth < v2ShadowCoord.z) ? 0.3*u_fShadowValy : 0.0;
+    //
+    //    float fPos = max(abs(v2TexCoord.x-0.1), abs(v2TexCoord.y+0.3));
+    //    fShadow   *= max(1.0 - max(fPos - 0.8, 0.0)*5.0, 0.0);
+    //}
+    //else
+    {
+        fShadow = 0.0;
+    }
+    
+    vec3 v3Diffuse = v4Texture.rgb * (1.0 - fShadow) * (1.3 * max(0.0, fBumpFactor) + 0.3);
+    
+    vec3 v3Specular;
+    if(fRefFactor > 0.0) v3Specular = vec3(0.3 * v4Texture.a * pow(fRefFactor, 25.0));
+                    else v3Specular = vec3(0.0);
+    
+    gl_FragColor = vec4(v3Diffuse + v3Specular, 1.0);
+}
---
+++ source/cOutdoor.h
@@ -0,0 +1,99 @@
+//////////////////////////////////////////////////////
+//*------------------------------------------------*//
+//| Part of Project One (http://www.maus-games.at) |//
+//*------------------------------------------------*//
+//| Released under the zlib License                |//
+//| More information available in the readme file  |//
+//*------------------------------------------------*//
+//////////////////////////////////////////////////////
+#pragma once
+#ifndef _P1_GUARD_OUTDOOR_H_
+#define _P1_GUARD_OUTDOOR_H_
+
+
+// ****************************************************************
+// outdoor definitions
+#define OUTDOOR_VIEW           (31)                                              // visible rows
+#define OUTDOOR_DETAIL         (6.0f)                                            // size of a block
+
+#define OUTDOOR_WIDTH          (31)                                              // vertices per row
+#define OUTDOOR_HEIGHT         (160)                                             // vertices per column
+#define OUTDOOR_HEIGHT_FULL    (OUTDOOR_HEIGHT + OUTDOOR_VIEW)                   // with double vertices
+
+#define OUTDOOR_BLOCKS_X       (OUTDOOR_WIDTH-1)                                 // blocks per row
+#define OUTDOOR_BLOCKS_Y       (OUTDOOR_HEIGHT_FULL-1)                           // blocks per column
+#define OUTDOOR_BLOCKS         (OUTDOOR_BLOCKS_X * OUTDOOR_BLOCKS_Y)             // number of all blocks
+
+#define OUTDOOR_PER_INDICES    (6)                                               // indices per block
+#define OUTDOOR_TOTAL_VERTICES (OUTDOOR_WIDTH * OUTDOOR_HEIGHT_FULL)             // total number of vertices
+#define OUTDOOR_TOTAL_INDICES  (OUTDOOR_PER_INDICES * OUTDOOR_BLOCKS)            // total number of indices
+
+#define OUTDOOR_RANGE (OUTDOOR_BLOCKS_X * OUTDOOR_PER_INDICES * OUTDOOR_VIEW)    // vertices to draw at once
+
+
+// ****************************************************************
+// outdoor-surface class
+class cOutdoor final : public coreObject3D
+{
+private:
+    struct sVertex
+    {
+        coreVector3 vPosition;   // vertex position
+        coreVector2 vTexture;    // texture coordinate
+        coreVector3 vNormal;     // normal vector
+        coreVector4 vTangent;    // additional tangent vector
+
+        constexpr_func sVertex()noexcept;
+    };
+
+
+private:
+    float m_afHeight[OUTDOOR_TOTAL_VERTICES];   // height data for height calculations
+
+    coreUint m_iRenderOffset;                   // current render offset
+    float    m_fMoveOffset;                     // current move offset
+
+    float m_fShadowStrength;                    // shadow intensity on the outdoor-surface
+
+
+public:
+    cOutdoor(const char* pcTextureTop, const char* pcTextureBottom, const coreByte& iAlgorithm, const float& fGrade)noexcept;
+    ~cOutdoor();
+
+    // render and move the outdoor-surface
+    void Render()override;
+    void Move  ()override;
+
+    // load outdoor geometry
+    void LoadGeometry(const coreByte& iAlgorithm, const float& fGrade);
+
+    // retrieve height value
+    float RetrieveHeight(const coreVector2& vPosition);
+
+    // set object properties
+    void        SetMoveOffset    (const float& fMoveOffset);
+    inline void SetShadowStrength(const float& fShadowStrength) {m_fShadowStrength = fShadowStrength;}
+
+    // get object properties
+    inline const coreUint& GetRenderOffset  ()const {return m_iRenderOffset;}
+    inline const float&    GetMoveOffset    ()const {return m_fMoveOffset;}
+    inline const float&    GetShadowStrength()const {return m_fShadowStrength;}
+
+
+private:
+    DISABLE_COPY(cOutdoor)
+};
+
+
+// ****************************************************************
+// constructor
+constexpr_func cOutdoor::sVertex::sVertex()noexcept
+: vPosition (coreVector3(0.0f,0.0f,0.0f))
+, vTexture  (coreVector2(0.0f,0.0f))
+, vNormal   (coreVector3(0.0f,0.0f,0.0f))
+, vTangent  (coreVector4(0.0f,0.0f,0.0f,0.0f))
+{
+}
+
+
+#endif // _P1_GUARD_OUTDOOR_H_
