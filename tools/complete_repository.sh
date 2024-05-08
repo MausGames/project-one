fb6fd38d91c2488f3f31246fde3758557fcb1f30
Martin Mauersics <revision@maus-games.at>
2024-05-08 16:42:21 +0200

    added tools

---
+++ tools/complete_repository.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+../../CoreEngine/tools/scripts/download_archive.sh project-one launcher "../executable"
+../../CoreEngine/tools/scripts/download_archive.sh project-one fonts "../executable/data/fonts"
+../../CoreEngine/tools/scripts/download_archive.sh project-one models "../executable/data/models"
+../../CoreEngine/tools/scripts/download_archive.sh project-one sounds "../executable/data/sounds"
+../../CoreEngine/tools/scripts/download_archive.sh project-one textures "../executable/data/textures"
